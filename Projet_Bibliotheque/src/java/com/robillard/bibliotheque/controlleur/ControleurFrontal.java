1f831c4ab0c98f23d4ad8011bd3a7f31375e7644
NicolasRob <robillardnic@gmail.com>
2017-10-21 19:50:14 -0400

    Ajout de la vue et de l'action d'ajout d'ouvrages

--- Projet_Bibliotheque/src/java/com/robillard/bibliotheque/controlleur/ControleurFrontal.java
+++ Projet_Bibliotheque/src/java/com/robillard/bibliotheque/controlleur/ControleurFrontal.java
@@ -69,6 +69,10 @@ public class ControleurFrontal extends HttpServlet {
                     r = this.getServletContext().getNamedDispatcher("AfficherGestionCatalogue");
                     r.forward(request, response);
                     break;
+                case "afficherAjoutOuvrage" :
+                    r = this.getServletContext().getNamedDispatcher("AfficherAjoutOuvrage");
+                    r.forward(request, response);
+                    break;
                 case "connection":
                     r = this.getServletContext().getNamedDispatcher("Connection");
                     r.forward(request, response);
