3b5ef4c2bef411e260038e678eb41a7262c2a639
NicolasRob <robillardnic@gmail.com>
2017-10-22 13:15:26 -0400

    Ajout de la vue et de l'action de consultation de suggestions

--- Projet_Bibliotheque/src/java/com/robillard/bibliotheque/controlleur/ControleurFrontal.java
+++ Projet_Bibliotheque/src/java/com/robillard/bibliotheque/controlleur/ControleurFrontal.java
@@ -85,6 +85,10 @@ public class ControleurFrontal extends HttpServlet {
                     r = this.getServletContext().getNamedDispatcher("AfficherSuggestion");
                     r.forward(request, response);
                     break;
+                case "afficherGestionSuggestion" :
+                    r = this.getServletContext().getNamedDispatcher("AfficherGestionSuggestion");
+                    r.forward(request, response);
+                    break;
                 case "connection":
                     r = this.getServletContext().getNamedDispatcher("Connection");
                     r.forward(request, response);
